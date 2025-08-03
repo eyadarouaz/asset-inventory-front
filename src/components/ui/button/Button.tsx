25a5eaa6ac1bfcdcd2725b28d3b4be34e63d2cc9
Eya <eyaadarouez@gmail.com>
2025-08-03 15:41:09 +0100

    Add new values for size and variant props in Button component

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -2,8 +2,8 @@ import React, { ReactNode, ButtonHTMLAttributes } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  size?: "sm" | "md";
-  variant?: "primary" | "outline";
+  size?: "xs" | "sm" | "md";
+  variant?: "primary" | "outline" | "text-error";
   startIcon?: ReactNode;
   endIcon?: ReactNode;
   className?: string;
@@ -20,6 +20,7 @@ const Button: React.FC<ButtonProps> = ({
   ...rest
 }) => {
   const sizeClasses = {
+    xs: "px-2 py-1 text-xs",
     sm: "px-4 py-3 text-sm",
     md: "px-5 py-3.5 text-sm",
   };
@@ -29,13 +30,14 @@ const Button: React.FC<ButtonProps> = ({
       "bg-brand-500 text-white shadow-theme-xs hover:bg-brand-600 disabled:bg-brand-300",
     outline:
       "bg-white text-gray-700 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 dark:bg-gray-800 dark:text-gray-400 dark:ring-gray-700 dark:hover:bg-white/[0.03] dark:hover:text-gray-300",
+    "text-error":
+      "bg-transparent text-red-600 hover:text-red-700 dark:text-red-400 dark:hover:text-red-300",
   };
 
   return (
     <button
-      className={`inline-flex items-center justify-center font-medium gap-2 rounded-lg transition ${className} ${sizeClasses[size]} ${variantClasses[variant]} ${
-        disabled ? "cursor-not-allowed opacity-50" : ""
-      }`}
+      className={`inline-flex items-center justify-center font-medium gap-2 rounded-lg transition ${className} ${sizeClasses[size]} ${variantClasses[variant]} ${disabled ? "cursor-not-allowed opacity-50" : ""
+        }`}
       disabled={disabled}
       {...rest}
     >
